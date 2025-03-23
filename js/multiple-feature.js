657d4f1c43ec36c3da1bc82c840b8708a1ec6c4d
imjoyhalder <joyhalder00113355@gamil.com>
2025-03-23 15:00:32 +0600

    Dynamicaly card added

--- js/multiple-feature.js
+++ js/multiple-feature.js
@@ -1,27 +1,61 @@
 
-const loadPetData = async() =>{
+const loadPetData = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/peddy/pets')
     const data = await res.json()
     console.log(data.pets);
     displayPateCard(data.pets)
 }
 
-const displayPateCard = (pateDetailsArray) =>{
+const displayPateCard = (pateDetailsArray) => {
     const cardContainer = document.getElementById('petsCardContainer')
-    
+    pateDetailsArray.forEach((item) => {
+        const petCard = document.createElement('div')
+        petCard.innerHTML = `
+            <figure class="">
+                        <img src= ${item.image} alt="Shoes" class="rounded-xl object-cover" />
+                    </figure>
+                    <div class="space-y-1 pb-3">
+                        <h1 class="font-bold text-xl">${item.pet_name}</h1>
+                        <div class="flex items-center gap-1">
+                            <img class="size-4" src="https://img.icons8.com/?size=24&id=82774&format=png" alt="">
+                            <h1>Breed:  ${item.breed}</h1>
+                        </div>
+                        <div class="flex items-center gap-1">
+                            <img class="size-5" src="https://img.icons8.com/?size=48&id=WGDB4PoaXTo5&format=png" alt="">
+                            <h1>Birth: ${item.date_of_birth}</h1>
+                        </div>
+                        <div class="flex items-center gap-1">
+                            <img class="size-4" src="https://img.icons8.com/?size=25&id=2LHzVg3k4AqG&format=png" alt="">
+                            <h1>Gender: ${item.gender}</h1>
+                        </div>
+                        <div class="flex items-center gap-1">
+                            <img class="size-4" src="https://img.icons8.com/?size=24&id=85801&format=png" alt="">
+                            <h1>Price: ${item.price}</h1>
+                        </div>
+                    </div>
+                    <hr class="border-gray-300 ">
+                    <div class=" flex justify-between items-center pt-3 border-collapse ">
+                        <button class="btn w-16 btn-active border-s-lime-500"><img class=" " src="https://img.icons8.com/?size=24&id=83997&format=png" alt=""></button>
+                        <button class="btn w-16 btn-active border-s-lime-500">Adopt</button>
+                        <button class="btn w-16 btn-active border-s-lime-500">Details</button>
+                    </div>
+        `
+        petCard.classList = "card px-3 pt-3 pb-3 w-72 bg-orange-50 "
+        cardContainer.appendChild(petCard)
+    })
 }
 
-const loadPetCategory = async() =>{
+const loadPetCategory = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories')
     const data = await res.json()
     console.log(data.categories);
     displayPetCategory(data.categories)
 }
 
-const displayPetCategory = (petCategoryArray) =>{
+const displayPetCategory = (petCategoryArray) => {
     const categoryContainer = document.getElementById('petCategoryContainer')
-    
-    petCategoryArray.forEach((item)=>{
+
+    petCategoryArray.forEach((item) => {
         const categoryBtn = document.createElement('div')
         categoryBtn.innerHTML = `
             <button class="btn btn-active w-36 h-14 rounded-2xl">
@@ -31,7 +65,8 @@ const displayPetCategory = (petCategoryArray) =>{
         `
         categoryContainer.appendChild(categoryBtn)
     })
-    
+
 }
 
 loadPetCategory()
+loadPetData()
